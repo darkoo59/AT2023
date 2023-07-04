0d0d8b6fa9a6911a88bc75b5efdc249d011d2910
Rade Stojanovic <stojanovicrade614@gmail.com>
2023-07-04 15:30:11 +0200

    notification actor setup

--- Go/notification/go.mod
+++ Go/notification/go.mod
@@ -1,3 +1,8 @@
 module github.com/AT-SmFoYcSNaQ/AT2023/Go/notification
 
 go 1.20
+
+require (
+	github.com/asynkron/goconsole v0.0.0-20160504192649-bfa12eebf716
+	github.com/asynkron/protoactor-go v0.0.0-20230322053818-66c886e5c8bd
+)
--- Go/notification/main.go
+++ Go/notification/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"github.com/AT-SmFoYcSNaQ/AT2023/Go/notification/messages"
+	console "github.com/asynkron/goconsole"
+	"github.com/asynkron/protoactor-go/actor"
+	"github.com/asynkron/protoactor-go/remote"
+)
 
+type NotificationActor struct{}
+
+func (*NotificationActor) Receive(context actor.Context) {
+	switch msg := context.Message().(type) {
+	case *messages.Notification:
+		println(context.Message().(*messages.Notification).Message)
+		context.Send(msg.Sender, &messages.Response{
+			SomeValue: "response",
+		})
+	}
+}
+
+func main() {
+	system := actor.NewActorSystem()
+	remoteConfig := remote.Configure("127.0.0.1", 8092)
+	remoting := remote.NewRemote(system, remoteConfig)
+	remoting.Start()
+
+	remoting.Register("notification-actor", actor.PropsFromProducer(func() actor.Actor { return &NotificationActor{} }))
+	console.ReadLine()
 }
