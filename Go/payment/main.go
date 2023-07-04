1449e820d580a7be8e61143e12fb214f71f49861
Darjan Rac <darjan21@uns.ac.rs>
2023-07-04 14:03:16 +0200

    import actor.proto error

--- Go/payment/main.go
+++ Go/payment/main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"github.com/asynkron/protoactor-go/actor"
+	"github.com/asynkron/protoactor-go/remote"
+)
+
+type PaymentActor struct {
+	remoting *remote.Remote
+	context  *actor.RootContext
+}
+
 func main() {
 
 }
