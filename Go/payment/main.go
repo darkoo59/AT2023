b4cac42a9bb64e9069a1cb9e1158ca13437968d4
Darjan Rac <darjan21@uns.ac.rs>
2023-07-07 12:04:06 +0200

    core functionality,might change format messages

--- Go/payment/main.go
+++ Go/payment/main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"fmt"
+	"github.com/AT-SmFoYcSNaQ/AT2023/Go/payment/messages/Go/messages"
 	console "github.com/asynkron/goconsole"
 	"github.com/asynkron/protoactor-go/actor"
 	"github.com/asynkron/protoactor-go/remote"
+	"time"
 )
+
+type PaymentReq struct {
+	Quantity       int32
+	PricePerItem   float32
+	OrderId        string
+	AccountNumber  string
+	AccountBalance float32
+}
 
 type PaymentActor struct {
 	remoting *remote.Remote
@@ -12,10 +23,78 @@ type PaymentActor struct {
 }
 
 func (actor *PaymentActor) Receive(context actor.Context) {
-
+	switch msg := context.Message().(type) {
+	case *messages.PaymentRequest:
+		paymentReq := PaymentReq{
+			Quantity:       msg.Quantity,
+			PricePerItem:   msg.PricePerItem,
+			OrderId:        msg.OrderId,
+			AccountBalance: msg.AccountBalance,
+			AccountNumber:  msg.AccountNumber,
+		}
+		actor.handlePaymentRequest(paymentReq, context.Self())
+	}
 }
 
-//func (actor *PaymentActor) notifyAboutPayment()
+func (actor *PaymentActor) handlePaymentRequest(paymentReq PaymentReq, self *actor.PID) {
+	fmt.Println("Received payment request")
+	totalPrice := paymentReq.PricePerItem * float32(paymentReq.Quantity)
+	paymentSuccessful := totalPrice <= paymentReq.AccountBalance
+
+	if !paymentSuccessful {
+		actor.sendPaymentInfo(paymentReq, paymentSuccessful)
+		actor.sendPaymentInfoNotification(paymentReq, paymentSuccessful)
+		return
+	}
+	paymentReq.AccountBalance -= totalPrice
+
+	actor.sendPaymentInfo(paymentReq, paymentSuccessful)
+	actor.sendPaymentInfoNotification(paymentReq, paymentSuccessful)
+}
+
+func (actor *PaymentActor) sendPaymentInfo(paymentReq PaymentReq, isSuccessful bool) {
+	fmt.Println("Sending payment info to order actor")
+	message := &messages.OrderPaymentInfo{
+		OrderId:        paymentReq.OrderId,
+		IsSuccessful:   isSuccessful,
+		AccountNumber:  paymentReq.AccountNumber,
+		AccountBalance: paymentReq.AccountBalance,
+	}
+
+	spawnResponse, err := actor.remoting.SpawnNamed("127.0.0.1:8090", "order-actor", "order-actor", time.Second)
+
+	if err != nil {
+		panic(err)
+	}
+
+	actor.context.Send(spawnResponse.Pid, message)
+	fmt.Println("Sent payment message to order actor!")
+}
+
+func (actor *PaymentActor) sendPaymentInfoNotification(paymentReq PaymentReq, isSuccessful bool) {
+	fmt.Println("Sending payment info to notification actor")
+
+	paymentMessage := ""
+
+	if isSuccessful {
+		paymentMessage = fmt.Sprintf("Payment for orderId %s was successful! New account balance is %.2f", paymentReq.OrderId, paymentReq.AccountBalance)
+	} else {
+		paymentMessage = fmt.Sprintf("Payment for orderId %s was not successful,account balance did not change.", paymentReq.OrderId)
+	}
+
+	spawnResponse, err := actor.remoting.SpawnNamed("127.0.0.1:8098", "notification-actor", "notification-actor", time.Second)
+
+	message := &messages.PaymentInfo{
+		PaymentMessage: paymentMessage,
+	}
+
+	if err != nil {
+		panic(err)
+	}
+
+	actor.context.Send(spawnResponse.Pid, message)
+	fmt.Println("Sent payment message to notification actor!")
+}
 
 func main() {
 
