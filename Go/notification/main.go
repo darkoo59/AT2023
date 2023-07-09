34293d177a6ea4688db2bcea64778526e9051e99
Rade Stojanovic <stojanovicrade614@gmail.com>
2023-07-09 18:55:21 +0200

    notifications done

--- Go/notification/main.go
+++ Go/notification/main.go
@@ -1,22 +1,38 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/AT-SmFoYcSNaQ/AT2023/Go/notification/messages"
-	console "github.com/asynkron/goconsole"
+	"github.com/AT-SmFoYcSNaQ/AT2023/Go/notification/socket"
 	"github.com/asynkron/protoactor-go/actor"
 	"github.com/asynkron/protoactor-go/remote"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
 
-type NotificationActor struct{}
+type NotificationActor struct {
+	hub *socket.Hub
+}
 
-func (*NotificationActor) Receive(context actor.Context) {
+type NotificationResponse struct {
+	Message string
+	Type    int32
+}
+
+func (h *NotificationActor) Receive(context actor.Context) {
 	switch msg := context.Message().(type) {
 	case *messages.Notification:
 		fmt.Println("Received notification: " + context.Message().(*messages.Notification).Message)
-
-		//TODO: SEND MESSAGE TO CUSTOMER ACTOR
-
+		client := (*h.hub).GetClient(msg.ReceiverId)
+		if client == nil {
+			return
+		}
+		data, _ := json.Marshal(&NotificationResponse{
+			Message: msg.Message,
+			Type:    msg.Type,
+		})
+		client.Send <- data
 		context.Send(msg.Sender, &messages.Response{
 			Message: "Notification sent.",
 		})
@@ -24,11 +40,32 @@ func (*NotificationActor) Receive(context actor.Context) {
 }
 
 func main() {
+	r := gin.Default()
+	r.Use(cors.New(corsConfig()))
+	hub := socket.NewHub()
+	go hub.Run()
+	r.GET("/:userId", func(c *gin.Context) {
+		socket.ServeWs(hub, c.Writer, c.Request, c.Param("userId"))
+	})
+
 	system := actor.NewActorSystem()
 	remoteConfig := remote.Configure("127.0.0.1", 8092)
 	remoting := remote.NewRemote(system, remoteConfig)
 	remoting.Start()
+	remoting.Register("notification-actor", actor.PropsFromProducer(func() actor.Actor {
+		return &NotificationActor{
+			hub: hub,
+		}
+	}))
 
-	remoting.Register("notification-actor", actor.PropsFromProducer(func() actor.Actor { return &NotificationActor{} }))
-	console.ReadLine()
+	r.Run(":10000")
+}
+
+func corsConfig() cors.Config {
+	corsConfig := cors.DefaultConfig()
+	corsConfig.AllowMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD"}
+	corsConfig.AllowHeaders = []string{"Content-Type", "Authorization"}
+	corsConfig.AllowAllOrigins = true
+	corsConfig.AllowCredentials = true
+	return corsConfig
 }
