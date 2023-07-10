7e6ecb5c06d0c6ee449b04a1b6af1125ef652abc
Damjan <dimitrijevicdamjandd@gmail.com>
2023-07-10 10:13:12 +0200

    Inventory actor

--- Go/inventory/go.mod
+++ Go/inventory/go.mod
@@ -1,3 +1,61 @@
 module github.com/AT-SmFoYcSNaQ/AT2023/Go/inventory
 
 go 1.20
+
+require (
+	github.com/AT-SmFoYcSNaQ/AT2023/Go/order v0.0.0-00010101000000-000000000000
+	github.com/asynkron/protoactor-go v0.0.0-20230703103118-df5e4f42621c
+	go.mongodb.org/mongo-driver v1.12.0
+)
+
+require (
+	github.com/Workiva/go-datastructures v1.0.53 // indirect
+	github.com/asynkron/gofun v0.0.0-20220329210725-34fed760f4c2 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/emirpasic/gods v1.18.1 // indirect
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/labstack/echo v3.3.10+incompatible // indirect
+	github.com/labstack/gommon v0.3.1 // indirect
+	github.com/lithammer/shortuuid/v4 v4.0.0 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
+	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
+	github.com/orcaman/concurrent-map v1.0.0 // indirect
+	github.com/prometheus/client_golang v1.14.0 // indirect
+	github.com/prometheus/client_model v0.3.0 // indirect
+	github.com/prometheus/common v0.39.0 // indirect
+	github.com/prometheus/procfs v0.9.0 // indirect
+	github.com/stretchr/testify v1.8.3 // indirect
+	github.com/twmb/murmur3 v1.1.6 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.2 // indirect
+	github.com/xdg-go/stringprep v1.0.4 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	go.opentelemetry.io/otel v1.12.0 // indirect
+	go.opentelemetry.io/otel/exporters/prometheus v0.35.0 // indirect
+	go.opentelemetry.io/otel/metric v0.35.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.12.0 // indirect
+	go.opentelemetry.io/otel/sdk/metric v0.35.0 // indirect
+	go.opentelemetry.io/otel/trace v1.12.0 // indirect
+	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
+	golang.org/x/exp v0.0.0-20220518171630-0b5c67f07fdf // indirect
+	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	google.golang.org/genproto v0.0.0-20230410155749-daa745c078e1 // indirect
+	google.golang.org/grpc v1.55.0 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
+)
+
+replace github.com/AT-SmFoYcSNaQ/AT2023/Go/order => ../order
--- Go/inventory/main.go
+++ Go/inventory/main.go
@@ -1,5 +1,148 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"strconv"
+	"time"
 
+	"github.com/AT-SmFoYcSNaQ/AT2023/Go/inventory/model"
+	"github.com/AT-SmFoYcSNaQ/AT2023/Go/order/messages"
+	"github.com/asynkron/protoactor-go/actor"
+	"github.com/asynkron/protoactor-go/cluster"
+	"github.com/asynkron/protoactor-go/cluster/clusterproviders/automanaged"
+	"github.com/asynkron/protoactor-go/cluster/identitylookup/disthash"
+	"github.com/asynkron/protoactor-go/remote"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+var (
+	dbUri           = "mongodb://localhost:27017"
+	itemsCollection *mongo.Collection
+)
+
+var (
+	collectionItems = []interface{}{
+		model.Item{Name: "Samsung Galaxy S23 Ultra", Price: 2000, Quantity: 50},
+		model.Item{Name: "Lenovo IP3", Price: 1477.99, Quantity: 20},
+		model.Item{Name: "Sony PlayStation 5", Price: 600.99, Quantity: 30},
+		model.Item{Name: "AMD Ryzen 5 5600x", Price: 301, Quantity: 40},
+		model.Item{Name: "Intel Core i5-11400F", Price: 180.99, Quantity: 100},
+		model.Item{Name: "Logitech G915", Price: 340.99, Quantity: 17},
+		model.Item{Name: "Razer Viper Mini", Price: 45, Quantity: 4},
+		model.Item{Name: "Xwave usb hub 4-port", Price: 10, Quantity: 323},
+		model.Item{Name: "Apple macbook pro 16", Price: 5000, Quantity: 2},
+	}
+)
+
+type InventoryActor struct {
+}
+
+func (act *InventoryActor) Receive(ctx actor.Context) {
+	switch msg := ctx.Message().(type) {
+	case *actor.Started:
+		log.Println("Inventory actor started")
+	case messages.CheckAvailability_Request:
+		available, item := CheckItemAvailability(msg.ItemId, int(msg.Quantity))
+		ctx.Send(msg.Sender, messages.CheckAvailability_Response{
+			OrderId:     "????????????????????????????",
+			IsAvailable: available,
+			Quantity:    msg.Quantity,
+			ItemName:    item.Name,
+			ItemPrice:   float32(item.Price),
+		})
+	}
+}
+
+func NewInventoryActor() actor.Actor {
+	return &InventoryActor{}
+}
+
+func CheckItemAvailability(itemId string, quantity int) (bool, *model.Item) {
+	itemObjectId, err := primitive.ObjectIDFromHex(itemId)
+	if err != nil {
+		panic("Invalid item object id")
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	var item model.Item
+	err = itemsCollection.FindOne(ctx, bson.D{{Key: "_id", Value: itemObjectId}}).Decode(&item)
+	if err != nil {
+		log.Printf("No item with id: %v", itemId)
+		return false, &item
+	}
+
+	if uint32(quantity) > item.Quantity {
+		log.Printf("There are no enough items, item id: %v", itemId)
+		return false, &item
+	}
+
+	return true, &item
+}
+
+func ConnectToDb() {
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(dbUri).SetServerAPIOptions(serverAPI)
+
+	client, err := mongo.Connect(context.TODO(), opts)
+
+	if err != nil {
+		panic(err)
+	}
+
+	pingErr := client.Ping(context.TODO(), readpref.Primary())
+	if pingErr != nil {
+		log.Fatal("Ping, mongoDB: ", err.Error())
+	}
+
+	log.Printf("Connected to mongo database")
+
+	itemsCollection = client.Database("inventory").Collection("items")
+
+}
+
+func SeedItems() {
+	itemsCollection.Drop(context.TODO())
+	result, err := itemsCollection.InsertMany(context.TODO(), collectionItems)
+	if err != nil {
+		panic(err)
+	}
+	log.Printf("\n%v items seeded successfully", len(result.InsertedIDs))
+}
+
+func main() {
+	port := 20001
+	port1 := 8098
+	if len(os.Args) > 2 {
+		port, _ = strconv.Atoi(os.Args[1])
+		port1, _ = strconv.Atoi(os.Args[2])
+	}
+
+	system := actor.NewActorSystem()
+	remoteConfig := remote.Configure("127.0.0.1", port)
+
+	cp := automanaged.NewWithConfig(1*time.Second, port1, "localhost:8098")
+	clusterKind := cluster.NewKind(
+		"inventory-actor",
+		actor.PropsFromProducer(NewInventoryActor),
+	)
+	lookup := disthash.New()
+	clusterConfig := cluster.Configure("cluster-inventory", cp, lookup, remoteConfig, cluster.WithKinds(clusterKind))
+	clust := cluster.New(system, clusterConfig)
+
+	clust.StartMember()
+	defer clust.Shutdown(false)
+
+	finishChan := make(chan os.Signal, 1)
+	signal.Notify(finishChan, os.Interrupt)
+
+	ConnectToDb()
+	SeedItems()
+
+	<-finishChan
 }
