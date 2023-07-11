ede0aa08694fd72fa172b580326fde59f5fc7cd8
Rade Stojanovic <stojanovicrade614@gmail.com>
2023-07-11 22:08:46 +0200

    item list

--- public/src/app/core/services/order.service.ts
+++ public/src/app/core/services/order.service.ts
@@ -1,26 +1,31 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
+import { GenericDataService } from 'src/app/shared/generic-data.service';
 import { Item } from 'src/app/shared/model';
 import { environment } from 'src/environments/environment';
 
 export interface CreateOrderDto {
   quantity: number;
   itemId: string;
-  price: number;
+  price?: number;
 }
 
 @Injectable({
   providedIn: 'root',
 })
-export class OrderService {
-  constructor(private http: HttpClient) {}
+export class OrderService extends GenericDataService<Item[]> {
+  constructor(private http: HttpClient) {
+    super()
+  }
 
   createOrder(data: CreateOrderDto): Observable<any> {
     return this.http.post(`${environment.apiUrl}/customer/order`, data)
   }
 
   getItems() : Observable<Item[]> {
-    return this.http.get<Item[]>(`${environment.apiUrl}/customer/items`);
+    return this.http.get<Item[]>(`${environment.apiUrl}/customer/items`).pipe(
+      tap(res => this.setData = res)
+    );
   }
 }
